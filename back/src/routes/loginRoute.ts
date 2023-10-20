a2c8d77a4e28089e1bb84a6fb599b9d9f814fd98
Guilherme <xxguilhermesoaresxx@gmail.com>
2023-10-20 18:36:41 -0300

    feat: create login route

---
+++ back/src/routes/loginRoute.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+import { loginController } from '../controllers/loginController';
+import { validateRequest } from '../middlewares/validateRequest'; 
+import { loginUserSchema } from '../schemas/userSchemas';
+
+export const loginRoute = Router();
+
+loginRoute.post('/', validateRequest(loginUserSchema), loginController);
