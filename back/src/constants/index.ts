c784820ccfa9ffd6c4053e1572812f796fbfe5a4
Guilherme <xxguilhermesoaresxx@gmail.com>
2023-10-20 18:36:53 -0300

    feat: create constants

---
+++ back/src/constants/index.ts
@@ -0,0 +1,3 @@
+// Armazenando variáveis aqui pq não consegui configurar o .env
+export const SECRET="OvQo56Cr4AcOpm7jCygk22jXfhLFwcbb"
+export const DATABASE_CONNECTION="mongodb+srv://admin:PdVXIT4pV2k3Rjoq@cge.gympqap.mongodb.net/?retryWrites=true&w=majority"
