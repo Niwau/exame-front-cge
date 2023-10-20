6be27afdd0f619703c7060a1b4112b396fed90e7
Guilherme <xxguilhermesoaresxx@gmail.com>
2023-10-20 20:48:23 -0300

    feat: add user controller

---
+++ back/src/controllers/userController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+import { User } from '../models/userModel';
+import { CreateUser } from '../schemas/userSchemas';
+import { createResponse } from '../utils/response';
+import { hashPassword } from '../utils/hash';
+
+export const createUserController = async (req: Request, res: Response) => {
+  const { email, password } = req.body as CreateUser;
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    return res.status(401).json(createResponse('User already exists'));
+  }
+
+  await new User({ email, password: await hashPassword(password) }).save();
+  return res.status(201).json(createResponse('User created.'));
+};
