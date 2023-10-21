6a713671d185ba7adad8bf08f7e4c37c1291f4f5
Guilherme <xxguilhermesoaresxx@gmail.com>
2023-10-21 12:54:34 -0300

    feat: configure providers

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -1,13 +1,18 @@
 import type { AppProps } from 'next/app';
+import { ToastContainer } from 'react-toastify';
 import Head from 'next/head';
+import '../styles/globals.css';
+import 'react-toastify/dist/ReactToastify.css';
+import { AuthContextProvider } from '@/contexts/AuthContex';
 
 export default function App({ Component, pageProps }: AppProps) {
-    return (
-        <>
-            <Head>
-                <title>CGE-RJ</title>
-            </Head>
-            <Component {...pageProps} />
-        </>
-    );
+  return (
+    <AuthContextProvider>
+      <Head>
+        <title>CGE-RJ</title>
+      </Head>
+      <Component {...pageProps} />
+      <ToastContainer theme='dark'/>
+    </AuthContextProvider>
+  );
 }
