8710a0e1ad9e94b7bac42da571e23ade647de3e0
Guilherme <xxguilhermesoaresxx@gmail.com>
2023-10-21 12:54:50 -0300

    feat: change bg color

--- front/src/pages/_document.tsx
+++ front/src/pages/_document.tsx
@@ -1,15 +1,15 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
-    return (
-        <Html lang="pt-br">
-            <Head>
-                <link rel="icon" href="/logo512.png" />
-            </Head>
-            <body>
-                <Main />
-                <NextScript />
-            </body>
-        </Html>
-    );
+  return (
+    <Html lang="pt-br">
+      <Head>
+        <link rel="icon" href="/logo512.png" />
+      </Head>
+      <body className='bg-dark-200'>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
