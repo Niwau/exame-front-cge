c401d82a954c3e11dd8b4aa63b1bd3603b3cd543
Guilherme <xxguilhermesoaresxx@gmail.com>
2023-10-22 15:26:23 -0300

    feat: create sidebar component

---
+++ front/src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,27 @@
+import { useAuthContext } from '@/contexts/AuthContex';
+import { SidebarButton, SidebarButtonContainer } from './SidebarButton';
+import { Basket, Cube, SignOut } from '@phosphor-icons/react';
+
+export const Sidebar = () => {
+
+  const { setIsAuthenticated  } = useAuthContext()
+
+  const logout = () => {
+    setIsAuthenticated(false)
+    localStorage.removeItem('token')
+  }
+
+  return (
+    <aside className="bg-dark-100 w-[64px] h-[100vh] flex flex-col gap-2 py-4 px-2">
+      <SidebarButton href="/products">
+        <Basket size={'90%'} color="white" />
+      </SidebarButton>
+      <SidebarButton href="/categories">
+        <Cube size={'90%'} color="white" />
+      </SidebarButton>
+      <SidebarButtonContainer className='mt-auto'>
+        <SignOut size={'90%'} color="white" onClick={logout} />
+      </SidebarButtonContainer>
+    </aside>
+  );
+};
