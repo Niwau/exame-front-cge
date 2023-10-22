a4ab7cbc4cf448de00dd6ef0df13e6cfcecb0f6b
Guilherme <xxguilhermesoaresxx@gmail.com>
2023-10-22 15:24:55 -0300

    feat: create input component

--- front/src/components/Input.tsx
+++ front/src/components/Input.tsx
@@ -1,21 +1,27 @@
-import { forwardRef } from 'react';
+import { forwardRef, } from 'react';
+import { ErrorMessage } from './ErrorMessage';
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   errorMessage?: string;
+  containerStyle?: React.HTMLAttributes<HTMLDivElement>['className'];
+  LeftIcon?: React.ReactNode;
 }
 
-export const Input = forwardRef<HTMLInputElement, InputProps>(({ className, errorMessage, ...rest }, ref) => {
+export const Input = forwardRef<HTMLInputElement, InputProps>(({ className, errorMessage, containerStyle, LeftIcon, ...rest }, ref) => {
   const invalid = errorMessage && 'outline-1 outline-danger outline';
 
   return (
-    <div className="flex flex-col gap-1">
+    <div className={`flex flex-col gap-1 relative ${containerStyle}`}>
+      <div className='absolute top-[50%] translate-y-[-50%] left-3'>
+        {LeftIcon}
+      </div>
       <input
         ref={ref}
         type="text"
-        className={`bg-dark-400 outline-2 text-white border-0 rounded-full py-2 px-4 focus:outline focus:outline-primary ${invalid} ${className}`}
+        className={`bg-dark-400 outline-2 text-white border-0 rounded-full py-2 pr-4 ${LeftIcon ? 'pl-9' : 'pl-4'} focus:outline focus:outline-primary ${invalid} ${className}`}
         {...rest}
       />
-      {invalid && <p className="text-danger font-normal">{errorMessage}</p>}
+      <ErrorMessage message={errorMessage}/>
     </div>
   );
 });
