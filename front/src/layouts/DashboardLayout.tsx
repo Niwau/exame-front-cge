54512f080321c7c269d49dca6e5c21b143b566a1
Guilherme <xxguilhermesoaresxx@gmail.com>
2023-10-23 16:00:27 -0300

    refactor: dashboard layout

--- front/src/layouts/DashboardLayout.tsx
+++ front/src/layouts/DashboardLayout.tsx
@@ -1,22 +1,14 @@
 import { Sidebar } from '@/components/Sidebar/Sidebar';
-import { useAuthContext } from '@/contexts/AuthContex';
-import Login from '@/pages';
 import { Children } from '@/types/Children';
+import { PrivateLayout } from './PrivateLayout';
 
 export default function DashboardLayout({ children }: Children) {
-
-  const { isAuthenticated } = useAuthContext()
-
-  if (!isAuthenticated) return (
-    <Login/>
-  )
-
   return (
-    <div className='flex'>
-      <Sidebar/>
-      <div className='p-8 flex flex-1'>
-        { children }
+    <PrivateLayout>
+      <div className="grid grid-cols-[64px_calc(100%-64px)]">
+        <Sidebar />
+        <div className="p-8 flex flex-1">{children}</div>
       </div>
-    </div>
+    </PrivateLayout>
   );
 }
