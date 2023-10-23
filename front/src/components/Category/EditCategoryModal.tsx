8e7457eb1b7fbacf30b03e79bdd4218af6a3a56f
Guilherme <xxguilhermesoaresxx@gmail.com>
2023-10-23 15:58:07 -0300

    feat: create edit category modal

--- front/src/components/Category/EditCategoryModal.tsx
+++ front/src/components/Category/EditCategoryModal.tsx
@@ -1,114 +1,50 @@
-import { useEffect, useState } from 'react';
-import { Button, DeleteButton, SaveButton } from '../Button';
-import { Pen, Plus } from '@phosphor-icons/react';
-import { Field } from '../Field';
-import { Input } from '../Input';
-import { Modal } from '../Modal';
-import { useForm } from 'react-hook-form';
+import { Modal } from '../Modals/Modal';
 import { api } from '@/services/api';
-import { zodResolver } from '@hookform/resolvers/zod';
 import { toast } from 'react-toastify';
 import { useSWRConfig } from 'swr';
 import { CreateCategory, createCategorySchema } from '@/schemas/categorySchemas';
-import { Confirm } from '../Confirm';
 import { CategoryInterface } from '@/types/CategoryInterface';
-import { isAxiosError } from 'axios';
+import { Form } from '../Form';
+import { categoryFields } from './AddCategoryModal';
+import { EditActions } from '../EditActions';
+import { useToggle } from '@/hooks/useToggle';
+import { ERRORS } from '@/constants/errors';
+import { EditButton } from '../Button';
 
 export const EditCategoryModal = (category: CategoryInterface) => {
-  const [isOpen, setIsOpen] = useState(false);
+  const { open, isOpen, close } = useToggle();
   const { mutate } = useSWRConfig();
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    reset
-  } = useForm<CreateCategory>({
-    resolver: zodResolver(createCategorySchema)
-  });
-
-  useEffect(() => {
-    reset({
-      color: category.color,
-      name: category.name
-    });
-  }, [reset, category]);
-
-  const open = () => {
-    setIsOpen(true);
+  const handleRequest = async (request: () => Promise<void>) => {
+    try {
+      await request();
+      close();
+      mutate('/categories');
+    } catch (error) {
+      toast.error(ERRORS.FAIL);
+    }
   };
 
-  const close = () => {
-    setIsOpen(false);
-    reset();
-  };
-
-  const updateCategory = async (id: string, category: CreateCategory) => {
-    await api.put(`/categories/${id}`, category);
-    toast.success('Categoria adicionada com sucesso!');
-    close();
-    mutate('/categories');
+  const updateCategory = async (newCategory: CreateCategory) => {
+    await handleRequest(() => api.put(`/categories/${category._id}`, newCategory));
   };
 
   const deleteCategory = async () => {
-    await api.delete(`/categories/${category._id}`);
-    toast.success('Categoria excluída com sucesso!');
-    close();
-    mutate('/categories');
+    await handleRequest(() => api.delete(`/categories/${category._id}`));
   };
 
-  const onSubmit = handleSubmit(async (form) => {
-    try {
-      await updateCategory(category._id, form);
-    } catch (e) {
-      if (isAxiosError(e)) {
-        toast.error('Ocorreu um erro ao atualizar a categoria');
-      } else {
-        toast.error('Ocorreu um erro ao atualizar a categoria');
-      }
-    }
-  });
+  const onSubmit = async (form: CreateCategory) => {
+    await updateCategory(form);
+  };
 
   return (
     <>
-      <button className="flex mx-auto" onClick={open}>
-        <Pen weight="duotone" size={24} />
-      </button>
+      <EditButton onClick={open} />
 
-      <Modal title="Nova Categoria" isOpen={isOpen} onClose={close}>
-        <form className="mt-4 flex flex-col gap-6" onSubmit={onSubmit}>
-          <Field>
-            <Field.Label>Nome</Field.Label>
-            <Input errorMessage={errors.name?.message} {...register('name')} placeholder="Ex: Alimentos" />
-          </Field>
-          <Field>
-            <Field.Label>Cor de destaque</Field.Label>
-            <Input
-              className="w-full"
-              type="color"
-              errorMessage={errors.color?.message}
-              {...register('color')}
-            />
-          </Field>
-          <div className="flex w-full gap-4">
-            <Confirm
-              onConfirm={deleteCategory}
-              render={({ open }) => (
-                <DeleteButton type="button" onClick={open}>
-                  Excluir
-                </DeleteButton>
-              )}
-            />
-            <Confirm
-              onConfirm={onSubmit}
-              render={({ open }) => (
-                <SaveButton type="button" onClick={open}>
-                  Salvar
-                </SaveButton>
-              )}
-            />
-          </div>
-        </form>
+      <Modal title="Editar Categoria" isOpen={isOpen} onClose={close}>
+        <Form schema={createCategorySchema} onSubmit={onSubmit} fields={categoryFields} defaultValues={category}>
+          <EditActions onDelete={deleteCategory} />
+        </Form>
       </Modal>
     </>
   );
