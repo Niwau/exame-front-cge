62e8a37d77ebbb38355833fcb45f439fb1463899
Guilherme <xxguilhermesoaresxx@gmail.com>
2023-10-23 15:58:35 -0300

    refactor: add product modal

--- front/src/components/Product/AddProductModal.tsx
+++ front/src/components/Product/AddProductModal.tsx
@@ -1,115 +1,98 @@
-import { useEffect, useState } from 'react';
-import { Button } from '../Button';
-import { Plus } from '@phosphor-icons/react';
-import { Field } from '../Field';
-import { Input } from '../Input';
-import { Modal } from '../Modal';
-import { Select } from '../Select';
-import { useForm } from 'react-hook-form';
+import { useEffect } from 'react';
+import { AddButton } from '../Button';
+import { Modal } from '../Modals/Modal';
 import { CreateProduct, createProductSchema } from '@/schemas/productSchema';
 import { APIResponse, api, useCategories } from '@/services/api';
-import { zodResolver } from '@hookform/resolvers/zod';
 import { toast } from 'react-toastify';
 import { useSWRConfig } from 'swr';
-import { isAxiosError } from 'axios'
+import { Form, FormField, Option } from '../Form';
+import { DefaultValues, UseFormReturn } from 'react-hook-form';
+import { isAxiosError } from 'axios';
+import { useToggle } from '@/hooks/useToggle';
+import { ERRORS } from '@/constants/errors';
+import { useErrorChecker } from '@/hooks/useErrorChecker';
+import { CategoryInterface } from '@/types/CategoryInterface';
 
 export const AddProductModal = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const { data, error } = useCategories(); // Busca as categorias no cache
+  const { open, isOpen, close } = useToggle();
+  const { data, error } = useCategories();
   const { mutate } = useSWRConfig();
+  useErrorChecker(error, 'Ocorreu um erro ao buscar as categorias');
 
-  useEffect(() => {
-    if (error) {
-      toast.error('Ocorreu um erro ao buscar as categorias');
-    }
-  }, [error])
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    reset,
-    setError
-  } = useForm<CreateProduct>({
-    defaultValues: {
-      category: '',
-      name: '',
-      price: 0,
-      quantity: 1
-    },
-    resolver: zodResolver(createProductSchema)
-  });
-
-  const open = () => {
-    setIsOpen(true);
+  const addProduct = async (product: CreateProduct) => {
+    await api.post('/products', product);
+    mutate('/products');
+    close();
   };
 
-  const close = () => {
-    setIsOpen(false);
-    reset();
-  };
-
-  const onSubmit = handleSubmit(async (form) => {
-    try {
-      await api.post('/products', form);
-      toast.success('Produto adicionado com sucesso!');
-      close();
-      mutate('/products');
-    } catch (e) {
-      if (isAxiosError<APIResponse<string>>(e)) {
-        if (e.response?.data.message === 'Product already exists.') {
-          setError('name', { message: 'Esse produto já está cadastrado' })
-        }
-      } else {
-        toast.error('Ocorreu um erro ao adicionar o produto');
+  const handleAddProductError = (e: unknown, { setError }: UseFormReturn<CreateProduct>) => {
+    if (isAxiosError<APIResponse<string>>(e)) {
+      if (e.response?.data.message == 'Product already exists.') {
+        setError('name', { message: 'Esse produto já existe' });
       }
+    } else {
+      toast.error(ERRORS.FAIL);
     }
-  });
+  };
+
+  const onSubmit = async (product: CreateProduct, methods: UseFormReturn<CreateProduct>) => {
+    try {
+      await addProduct(product);
+    } catch (error) {
+      handleAddProductError(error, methods);
+    }
+  };
+
+  const fields = getProductFormFields(data?.data);
 
   return (
     <>
-      <Button onClick={open} leftIcon={<Plus weight="bold" />}>
-        Novo Produto
-      </Button>
-
+      <AddButton onClick={open}>Novo Produto</AddButton>
       <Modal title="Novo Produto" isOpen={isOpen} onClose={close}>
-        <form className="mt-4 flex flex-col gap-6" onSubmit={onSubmit}>
-          <Field>
-            <Field.Label>Nome</Field.Label>
-            <Input errorMessage={errors.name?.message} {...register('name')} placeholder="Ex: All Star" />
-          </Field>
-          <Field>
-            <Field.Label>Preço</Field.Label>
-            <Input
-              type="number"
-              step={0.01}
-              errorMessage={errors.price?.message}
-              {...register('price', { setValueAs: (v) => parseFloat(v) })}
-              placeholder="Ex: 12.00"
-            />
-          </Field>
-          <Field>
-            <Field.Label>Quantidade</Field.Label>
-            <Input
-              type="number"
-              errorMessage={errors.quantity?.message}
-              {...register('quantity', { setValueAs: (v) => parseInt(v) })}
-              placeholder="Ex: 2"
-            />
-          </Field>
-          <Field>
-            <Field.Label>Categoria</Field.Label>
-            <Select errorMessage={errors.category?.message} {...register('category')}>
-              {data?.data.map((category, index) => (
-                <option key={index} value={category._id}>
-                  {category.name}
-                </option>
-              ))}
-            </Select>
-          </Field>
-          <Button leftIcon={<Plus />}>Adicionar Produto</Button>
-        </form>
+        <Form defaultValues={defaultValues} onSubmit={onSubmit} schema={createProductSchema} fields={fields}>
+          <AddButton>Adicionar Produto</AddButton>
+        </Form>
       </Modal>
     </>
   );
+};
+
+export const getProductFormFields = (categories?: CategoryInterface[]): FormField<CreateProduct>[] => {
+  const categoryOptions: Option[] =
+    categories?.map((category) => ({ label: category.name, value: category._id })) ?? [];
+
+  return [
+    {
+      label: 'Nome',
+      name: 'name',
+      placeholder: 'Ex: Arroz'
+    },
+    {
+      label: 'Preço',
+      name: 'price',
+      type: 'number',
+      registerOptions: { setValueAs: (price) => parseFloat(price) },
+      step: 0.01,
+      placeholder: 'Ex: 12,00'
+    },
+    {
+      label: 'Quantidade',
+      name: 'quantity',
+      type: 'number',
+      placeholder: 'Ex: 1',
+      registerOptions: { setValueAs: (quantity) => parseInt(quantity) }
+    },
+    {
+      label: 'Categoria',
+      name: 'category',
+      options: categoryOptions
+    }
+  ];
+};
+
+const defaultValues: DefaultValues<CreateProduct> = {
+  category: '',
+  name: '',
+  price: 0,
+  quantity: 1
 };
